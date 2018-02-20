edf26d311c82705c77b06ea51a4cc00a5bf71759
Pine Mizune <pinemz@gmail.com>
2018-02-21 02:02:30 +0900

    feat(buildSrc): add generate script [wip]

--- buildSrc/src/main/groovy/com/github/holidayjp/buildscript/GeneratorTask.groovy
+++ buildSrc/src/main/groovy/com/github/holidayjp/buildscript/GeneratorTask.groovy
@@ -1,7 +1,18 @@
 package com.github.holidayjp.buildscript
 
+import org.yaml.snakeyaml.Yaml
+
+import java.nio.file.Paths
+
 class GeneratorTask {
-    static run() {
+    static run(String rootDir) {
+        def parser = new Yaml()
+        def dataSheetFile = Paths.get(rootDir, 'holiday_jp/holidays_detailed.yml').toFile()
+        def holidaysDetailed = parser.load(dataSheetFile.text)
+        def holidays = holidaysDetailed.values().sort { lhs, rhs -> lhs.date <=> rhs.date }
+
+
+        def outputFile = Paths.get(rootDir, "holidayjp")
 
     }
 }
