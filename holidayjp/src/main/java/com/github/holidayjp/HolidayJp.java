481da20f84396c0ac171e37bf7288a90d86ffc59
Pine Mizune <pinemz@gmail.com>
2018-02-21 01:25:08 +0900

    feat(repository): add empty project

---
+++ holidayjp/src/main/java/com/github/holidayjp/HolidayJp.java
@@ -0,0 +1,22 @@
+package com.github.holidayjp;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * HolidayJp
+ * Created by pine on 2018/02/21.
+ */
+
+public final class HolidayJp {
+    private HolidayJp() {
+    }
+
+    public static boolean isHoliday(Date date) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static boolean isHoliday(Calendar calendar) {
+        throw new UnsupportedOperationException();
+    }
+}
